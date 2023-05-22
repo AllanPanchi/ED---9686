4c4d6d3c049381c62ef18ee434e45d0ce43d82e0
Alejo Andrade <02alejo20@gmail.com>
2023-05-21 22:34:42 -0500

    Carpetas renombradas

---
+++ Alejo/C5_Memoria_Dinamica/main.cpp
@@ -0,0 +1,24 @@
+/*
+UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+Materia: Estructura de Datos
+NRC: 9686
+Integrantes: Alex Trejo
+			 Allan Panchi
+			 Sebastian Verdugo
+			 Alejandro Andrade
+Fecha de inicio: 19/05/2023
+Fecha de modificación: 21/05/2023
+
+Memoria Dinamica
+*/
+
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+    system("pause");
+    
+    return 0;
+}
