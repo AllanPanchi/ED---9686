3e37da680be462ad32e0d6a8a73222a666dfaca4
Alex Fernando Trejo Duque <aftrejo@espe.edu.ec>
2023-07-17 09:10:11 -0500

    Etiquetas

--- proyectos/Parcial 2 Alex-Alejo/Polaca/Lista.h
+++ proyectos/Parcial 2 Alex-Alejo/Polaca/Lista.h
@@ -1,4 +1,15 @@
+/*
+UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+Materia: Estructura de Datos
+NRC: 9686
+Integrantes:    Alejandro Andrade
+                Alex Trejo 
+                
+Fecha de inicio: 30/06/2023
+Fecha de modificación: 16/07/2023
 
+Proyecto sobre Calculadora polaca infija, posfija y prefija
+*/
 #include "Nodo.cpp"
 
 template <typename T>
